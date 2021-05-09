353fde85085f255d375f6767404110c5b966919c
Simon Mestdagh <simonmestdagh2001@gmail.com>
2021-05-09 18:10:16 +0200

    fixed cors problems

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -8,6 +8,11 @@ const app: Application = express()
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 let value: number = 0.0;
 
   
